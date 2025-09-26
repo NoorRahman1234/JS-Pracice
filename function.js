df11a6dfb0c2117f152ae5fccba4d55d0012219d
NoorRahman1234 <rahmanafr0123@gmail.com>
2025-09-26 15:30:32 +0500

    Class Task   Vilidation  on 26/9/25

--- function.js
+++ function.js
@@ -67,63 +67,96 @@
 
 
    // If else Condidion    Date 26/09/25
-let age=18
-    let userage= prompt("Enter your age") 
-    if(userage>=20){
-        console.log("You can drive")
+// let age=18
+//     let userage= prompt("Enter your age") 
+//     if(userage>=20 && userage<=60){
+//         console.log("You can drive")
 
-    }else if(userage<=5){
-        console.log(" You cannot drive You are kid")
-    }else if(userage<=10){
-        console.log(" You cannot drive You are not an adult")
+//     }else if(userage<=5 && userage >=0){
+//         console.log(" You cannot drive You are kid")
+//     }else if(userage<=10 && userage >=5){
+//         console.log(" You cannot drive You are not an adult")
 
-    }else if (userage<=19){
-        console.log(" You can only ride bysycle You a Tenager")
+//     }else if (userage>=11 && userage<=12){
+//       console.log("You are a school boy")
+//     }
+//     else if (userage<=19 && userage >=13){
+//         console.log(" You can only ride bysycle You are Tenager")
 
-    }else{
-        console.log("you need training first")
-    }
+//     }else{
+//         console.log("You are overage you are prohibited to drive")
+//     }
 
 
     // Switch condition 
-  let day = +prompt("Enter a number (1-7) for the day of the week:");
+//   let day = +prompt("Enter a number (1-7) for the day of the week:");
 
-switch(day) {
-  case 1:
-    console.log("Monday");
-    break;
-  case 2:
-    console.log("Tuesday");
-    break;
-  case 3:
-    console.log("Wednesday");
-    break;
-  case 4:
-    console.log("Thursday");
-    break;
-  case 5:
-    console.log("Friday");
-    break;
-  case 6:
-    console.log("Saturday");
-    break;
-  case 7:
-    console.log("Sunday");
-    break;
-  default:
-    console.log("Invalid number! Please enter 1–7.");
-}
+// switch(day) {
+//   case 1:
+//     console.log("Monday");
+//     break;
+//   case 2:
+//     console.log("Tuesday");
+//     break;
+//   case 3:
+//     console.log("Wednesday");
+//     break;
+//   case 4:
+//     console.log("Thursday");
+//     break;
+//   case 5:
+//     console.log("Friday");
+//     break;
+//   case 6:
+//     console.log("Saturday");
+//     break;
+//   case 7:
+//     console.log("Sunday");
+//     break;
+//   default:
+//     console.log("Invalid number! Please enter 1–7.");
+// }
 
 
 // Optimize code
 
-let day = +prompt("Enter a number from (1-7) for a day of the week:");
+// let day = +prompt("Enter a number from (1-7) for a day of the week:");
 
-let days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+// let days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
 
-if(day >= 1 && day <= 7) {
-  console.log(days[day - 1]);  // -1 because array index starts from 0
-} else {
-  console.log("Invalid number! Please enter 1–7 for a day of the week");
-}
+// if(day >= 1 && day <= 7) {
+//   console.log(days[day - 1]);  // -1 because array index starts from 0
+// } else {
+//   console.log("Invalid number! Please enter 1–7 for a day of the week");
+// }
+
+
+
+// Date 26 /9/25 Class Task  Vilidation
+    let userInput = prompt("Enter you number")
+    if(isNaN(userInput)){
+    console.log("You entered letter")
+    }else if(isNaN(userInput>0)){
+    console.log("This is a number")
+    }else if(userInput<=33 && userInput>=0){
+        console.log("You are fail")
+    }else if(userInput>33 && userInput<50){
+        console.log("you got D grade")
+    }else if(userInput>=50 && userInput<70){
+        console.log("You got grade B")
+    }else if (userInput>=70 && userInput<90){
+        console.log("You got grade A")
+    }else if(userInput>=90 && userInput<100){
+        console.log("you got grade A+")
+    }else if(userInput<0){
+        console.log(" please Enter positive number")
+    }
+    else if(userInput > 100){
+    console.log("please enter number less than 100")
+       }
+     else{
+    console.log("This is a  number")
+
+    }
+
 
